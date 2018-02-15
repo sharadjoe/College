34e9a3b39aa78ad965045a9c8930672e56b6738e
Sharad Joe <sharadjoe48@gmail.com>
2018-02-15 15:34:07 +0530

    Just added the link to the website

--- SIGINT.c
+++ SIGINT.c
@@ -1,11 +1,20 @@
+https://gist.github.com/leehambley/5589953
+
 #include<stdio.h>
 #include<signal.h>
 #include<unistd.h>
+#incldue<sys/types.h>
+#include<sys/wait.h>
 
 void signal_handler(int signo)
 {
     if(signo== SIGINT)
         printf("Recieved Sigint");
+}
+
+void handler(int sig)
+{
+    pid_t pid;    
 }
 
 int main(void)
