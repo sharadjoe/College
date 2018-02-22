6e398571736ec8215ee102f91236a048206ce2fd
melbin1997 <melbin1997@gmail.com>
2018-02-22 15:55:54 +0530

    Add files via upload

--- pipe1.c
+++ pipe1.c
@@ -6,7 +6,7 @@
 void main()
 {
 int pipe1[2],pipe2[2],p;
-char buff[100],buff2[100];
+char buff[100],buff2[100],a1[100],a2[100];
 pipe(pipe1);
 pipe(pipe2);
 p=fork();
@@ -15,8 +15,10 @@ if(p==0){
 	printf("This is child process\n");
 	close(pipe1[1]);
 	close(pipe2[0]);
-	printf("Writing hi from child process\n");
-	write(pipe2[1],"hi",3);
+	printf("Enter input from child process : ");
+	scanf("%s", a2);
+	printf("Writing %s from child process\n",a2);
+	write(pipe2[1],a2,sizeof(a2));
 	read(pipe1[0],buff,sizeof(buff));
 	printf("Child process read %s\n", buff);
 	close(pipe1[0]);	
@@ -26,8 +28,10 @@ else{
 	printf("This is parent process\n");
 	close(pipe1[0]);
 	close(pipe2[1]);
-	printf("Writing hello world from parent process\n");
-	write(pipe1[1],"hello world",12);
+	printf("Enter input from parent process : ");
+	scanf("%s", a1);
+	printf("Writing %s from parent process\n",a1);
+	write(pipe1[1],a1,sizeof(a1));
 	read(pipe2[0],buff2,sizeof(buff2));
 	printf("Parent process read %s\n",buff2);
 	close(pipe1[1]);
